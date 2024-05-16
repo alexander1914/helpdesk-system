0c817b5cdbe791ea994d93fc66aebe2ef83f36a5
alexander1914 <alexander.oliveira99@gmail.com>
2024-05-16 18:17:20 +0100

    Implementando um perfil de dev para o back-end

--- helpdesk/src/main/java/br/com/system/helpdesk/HelpdeskApplication.java
+++ helpdesk/src/main/java/br/com/system/helpdesk/HelpdeskApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelpdeskApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
-
     }
 }
--- helpdesk/src/main/java/br/com/system/helpdesk/config/DevConfig.java
+++ helpdesk/src/main/java/br/com/system/helpdesk/config/DevConfig.java
@@ -2,19 +2,26 @@ package br.com.system.helpdesk.config;
 
 import br.com.system.helpdesk.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
     @Bean
-    public void instanciaDB() {
-        this.dbService.instanciaDB();
+    public boolean instanciaDB() {
+        if(value.equals("create")) {
+            this.dbService.instanciaDB();
+        }
+        return false;
     }
 }
