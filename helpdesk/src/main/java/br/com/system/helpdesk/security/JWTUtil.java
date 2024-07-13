81e9872844caa1c658692ea608aae4502867daae
alexander1914 <alexander.oliveira99@gmail.com>
2024-07-13 16:28:03 +0100

    Deploy para usar h2 como base local com token maior tempo

--- helpdesk/src/main/java/br/com/system/helpdesk/security/JWTUtil.java
+++ helpdesk/src/main/java/br/com/system/helpdesk/security/JWTUtil.java
@@ -41,7 +41,10 @@ public class JWTUtil {
 
     private Claims getClaims(String token) {
         try {
-            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJwt(token).getBody();
+            return Jwts.parser()
+                    .setSigningKey(secret.getBytes())
+                    .parseClaimsJws(token)
+                    .getBody();
         } catch (Exception e) {
             return null;
         }
